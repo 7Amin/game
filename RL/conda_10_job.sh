e6a875e46ec5036e57922f89604ea5906329e8dc
amin.karimi <amin.karimi1992@gmail.com>
2023-01-11 21:22:27 -0500

    add RL

--- RL/conda_10_job.sh
+++ RL/conda_10_job.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --account=PAS0536
-#SBATCH --job-name=transformer_SpaceInvaders-v4
+#SBATCH --job-name=dqn_SpaceInvaders-v4
 #SBATCH --time=4:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
@@ -14,5 +14,5 @@ source activate local
 #pip install -r requirements.txt
 #pip install gym[atari,accept-rom-license,all]
 cd /users/PAS0536/aminr8/game/RL
-PYTHONPATH=. python3 runner.py -m "transformer" -u 24 -g "SpaceInvaders-v4" -b 8
+PYTHONPATH=. python3 runner.py -m "dqn" -u 24 -g "SpaceInvaders-v4" -b 8
 
